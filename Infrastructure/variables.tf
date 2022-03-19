ee9704bde5b8e6634f746a33df475adbce6b842b
AraujoDi <diaraujo_@hotmail.com>
2022-03-19 17:49:30 -0300

    Add Ec2, iam, variables e remoção de s3_files

--- Infrastructure/variables.tf
+++ Infrastructure/variables.tf
@@ -5,3 +5,19 @@ variable "base_bucket_name" {
 variable "aws_region" {
     default = "us-east-1"
 }
+
+variable "lambda_function_name"{
+    default = "diego-igti-teste"
+}
+
+variable "key_pair_name" {
+    default = "diego-igti-key-pair"
+}
+
+variable "airflow_subnet_id" {
+    default = "subnet-08794332fbd6959c1"
+}
+
+variable "vpc_id" {
+    default = "vpc-0d59dc3c23f5d9fd2"  
+}
