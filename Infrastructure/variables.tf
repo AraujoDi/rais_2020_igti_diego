b9b2d3c9302b04913308762d236457dc4062fe94
AraujoDi <diaraujo_@hotmail.com>
2022-03-19 01:30:28 -0300

    Correção da varible

--- Infrastructure/variables.tf
+++ Infrastructure/variables.tf
@@ -1,7 +1,7 @@
-variables "base_bucket_name" {
+variable "base_bucket_name" {
     default = "dtlk-diego-igti-rais-tf"
 }
 
-variables "aws_region" {
+variable "aws_region" {
     default = "us-east-1"
 }
